905905fe736f999705ffdbd43b9e1746ccbe31fd
fabsoto <fabiandesoto88@gmail.com>
2017-04-30 19:20:25 -0500

    Added button, making it work is something else altogether

--- app/src/main/java/com/example/gabriella/chatizoproject/MessagesScreen.java
+++ app/src/main/java/com/example/gabriella/chatizoproject/MessagesScreen.java
@@ -1,13 +1,20 @@
 package com.example.gabriella.chatizoproject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MessagesScreen extends Fragment {
-
+    private FloatingActionButton composebutton;
 
     public static MessagesScreen newInstance() {
         MessagesScreen frag = new MessagesScreen();
@@ -24,4 +31,18 @@ public class MessagesScreen extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_messages_screen, container, false);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
